b25188fe699c0c4164e2c870cdce29cc6a8e2b91
Santos <sg21894@gmail.com>
2022-04-29 23:34:48 -1000

    Updates

---
+++ precache-manifest.b75ded81aab38aba6a2ef4ca5453d5a2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a5e53114cb84e46bc42e2d4cbf341bd1",
+    "url": "/San-toast/react-frontend-dev-portfolio/index.html"
+  },
+  {
+    "revision": "f3b519c7d7dc0d3d23de",
+    "url": "/San-toast/react-frontend-dev-portfolio/static/css/2.d64471e2.chunk.css"
+  },
+  {
+    "revision": "df3a3246b9796382ac30",
+    "url": "/San-toast/react-frontend-dev-portfolio/static/css/main.baf12b2e.chunk.css"
+  },
+  {
+    "revision": "f3b519c7d7dc0d3d23de",
+    "url": "/San-toast/react-frontend-dev-portfolio/static/js/2.bd90c163.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "/San-toast/react-frontend-dev-portfolio/static/js/2.bd90c163.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "df3a3246b9796382ac30",
+    "url": "/San-toast/react-frontend-dev-portfolio/static/js/main.e945159e.chunk.js"
+  },
+  {
+    "revision": "11296efbecb4a04ada5d",
+    "url": "/San-toast/react-frontend-dev-portfolio/static/js/runtime-main.a854b64f.js"
+  }
+]);
